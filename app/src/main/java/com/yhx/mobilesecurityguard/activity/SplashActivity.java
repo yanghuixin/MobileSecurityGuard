80c69e42f83ee6aaf58a31105173bce9bc6a144f
sunshineCEO <1298435317@qq.com>
2017-12-15 22:53:13 +0800

    版本更新

--- app/src/main/java/com/yhx/mobilesecurityguard/activity/SplashActivity.java
+++ app/src/main/java/com/yhx/mobilesecurityguard/activity/SplashActivity.java
@@ -1,15 +1,23 @@
 package com.yhx.mobilesecurityguard.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yhx.mobilesecurityguard.R;
 import com.yhx.mobilesecurityguard.utils.ToastUtils;
 import com.yhx.mobilesecurityguard.utils.VersionInformationUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * 闪屏页面
@@ -22,7 +30,30 @@ import com.yhx.mobilesecurityguard.utils.VersionInformationUtils;
 public class SplashActivity extends Activity {
 
     private TextView tv_splash_edition;
+    //10.0.2.2是预留ip，供模拟器访问PC的服务器
+    private final static String JSONURL = "http://192.168.2.101:8080/update66.json";
+    private final static int CODE_UPDATE_DIALOG = 1;
+    private final static int CODE_ENTER_HOME = 2;
 
+    //版本信息
+    private String versionName;
+    private int versionCode;
+    private String des;
+    private String url;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case CODE_UPDATE_DIALOG:
+                    showUpdateDialog();
+                    break;
+                case CODE_ENTER_HOME:
+                    enterHome();
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +61,68 @@ public class SplashActivity extends Activity {
 
         tv_splash_edition = findViewById(R.id.tv_splash_edition);
         tv_splash_edition.setText("版本名：" + VersionInformationUtils.getVersionName(this));
+        checkVersion();
     }
 
+    private void checkVersion(){
+        new Thread(){
+            @Override
+            public void run() {
+                String result = VersionInformationUtils.getResult(JSONURL);
+                Message msg = Message.obtain();
+                try {
+                    //解析json
+                    JSONObject json = new JSONObject(result);
+                    versionName = json.getString("versionName");
+                    versionCode = json.getInt("versionCode");
+                    des = json.getString("des");
+                    url = json.getString("url");
+                    if (VersionInformationUtils.getVersionCode(SplashActivity.this) < versionCode){
+                        //showUpdateDialog();
+                        msg.what = CODE_UPDATE_DIALOG;
+                    } else{
+                        msg.what = CODE_ENTER_HOME;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        Thread.sleep(2000);//强制等待一段时间
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.sendMessage(msg);
+                }
+            }
+        }.start();
+    }
+
+    /**
+     * 升级弹窗
+     */
+    private void showUpdateDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("发现新版本" + versionName);
+        builder.setMessage(des);
+        builder.setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("以后再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * 跳转到主页面
+     */
+    private void enterHome(){
+        startActivity(new Intent(this, HomeActivity.class));
+    }
 }
--- app/src/main/java/com/yhx/mobilesecurityguard/utils/VersionInformationUtils.java
+++ app/src/main/java/com/yhx/mobilesecurityguard/utils/VersionInformationUtils.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,7 +30,6 @@ public class VersionInformationUtils {
             //根据包名，获取相关信息
             PackageInfo packageInfo = pm.getPackageInfo(content.getPackageName(), 0);
             String versionName = packageInfo.versionName;//版本名称
-            int versionCode = packageInfo.versionCode;//版本号
             return versionName;
         } catch (PackageManager.NameNotFoundException e) {
             //包名未找到异常
@@ -37,30 +39,46 @@ public class VersionInformationUtils {
     }
 
     /**
-     * 检查版本
+     * 获取版本号
+     * @param content
+     * @return
      */
-    public static void checkVersion(){
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    //10.0.2.2是预留ip，供模拟器访问PC的服务器
-                    HttpURLConnection conn= (HttpURLConnection) new URL("http://10.0.2.2:8080/update.json").openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setConnectTimeout(2000);//连接超时
-                    conn.setReadTimeout(2000);//读取超时，连接上了，服务器不给响应
-                    conn.connect();
+    public static int getVersionCode(Context content){
+        PackageManager pm = content.getPackageManager();//包管理器
+        try {
+            //根据包名，获取相关信息
+            PackageInfo packageInfo = pm.getPackageInfo(content.getPackageName(), 0);
+            int versionCode = packageInfo.versionCode;//版本号
+            return versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            //包名未找到异常
+            e.printStackTrace();
+        }
+        return -1;
+    }
 
-                    int responseCode = conn.getResponseCode();
-                    if (responseCode == 200){
-                        InputStream in = conn.getInputStream();
-                        String result = StreamUtils.streamToString(in);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    /**
+     * 获取GET请求的数据
+     */
+    public static String getResult(final String url){
+        HttpURLConnection conn= null;
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(2000);//连接超时
+            conn.setReadTimeout(2000);//读取超时，连接上了，服务器不给响应
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200){
+                InputStream in = conn.getInputStream();
+                String result = StreamUtils.streamToString(in);
+                return result;
             }
-        }.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
