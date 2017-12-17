99b0b6d7434b443df9bed22a77622977d3246374
sunshineCEO <1298435317@qq.com>
2017-12-18 01:22:52 +0800

    设置向导页优化

--- app/src/main/java/com/yhx/mobilesecurityguard/activity/AntiTheftSetupActivity.java
+++ app/src/main/java/com/yhx/mobilesecurityguard/activity/AntiTheftSetupActivity.java
@@ -1,8 +1,11 @@
 package com.yhx.mobilesecurityguard.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.yhx.mobilesecurityguard.R;
 
@@ -15,5 +18,12 @@ public class AntiTheftSetupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anti_theft_setup);
+        Button bt_anti_theft_setup_next = findViewById(R.id.bt_anti_theft_setup_next);
+        bt_anti_theft_setup_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AntiTheftSetupTwoActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/yhx/mobilesecurityguard/activity/AntiTheftSetupTwoActivity.java
@@ -0,0 +1,18 @@
+package com.yhx.mobilesecurityguard.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yhx.mobilesecurityguard.R;
+
+/**
+ * 设置向导页面
+ */
+public class AntiTheftSetupTwoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anti_theft_setup_two);
+    }
+}
